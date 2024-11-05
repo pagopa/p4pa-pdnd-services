857273644689a537fc059270a1c87b1fc7d16691
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2024-11-05 18:06:00 +0100

    minor change

--- .github/terraform/99_locals.tf
+++ .github/terraform/99_locals.tf
@@ -4,28 +4,27 @@ locals {
     CreatedBy   = "Terraform"
     Environment = var.env
     Owner       = upper(var.prefix)
-    Source      = "" # Repository URL
-    CostCenter  = ""
+    Source      = "https://github.com/pagopa/p4pa-pdnd-services" # Repository URL
+    CostCenter  = "TS310 - PAGAMENTI & SERVIZI"
   }
 
   # Repo
   github = {
     org        = "pagopa"
-    repository = "" # Repository Name
+    repository = "p4pa-pdnd-services" # Repository Name
   }
 
-  env_secrets = {
-    ENV_SECRET = "data.azurerm_key_vault_secret.CHANGE_ME.value"
-  }
-  env_variables = {
-    ENV_VARIABLE = "ENV_VARIABLE"
-  }
+  env_secrets   = {}
+  env_variables = {}
 
   repo_secrets = var.env_short == "p" ? {
-    SECRET = "SECRET"
+    SONAR_TOKEN = data.azurerm_key_vault_secret.sonar_token[0].value
   } : {}
+
   repo_env = var.env_short == "p" ? {
-    ENV_VARIABLE = "ENV_VARIABLE"
+    SONARCLOUD_PROJECT_NAME = "p4pa-pdnd-services"
+    SONARCLOUD_PROJECT_KEY  = "pagopa_p4pa-pdnd-services"
+    SONARCLOUD_ORG          = "pagopa"
   } : {}
 
   map_repo = {
