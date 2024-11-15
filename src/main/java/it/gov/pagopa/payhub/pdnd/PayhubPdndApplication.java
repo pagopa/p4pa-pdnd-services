22a7e686d264a838d4c74f6b26070db6aec2f59b
Antonio Calò <antonio.calo@nttdata.com>
2024-11-15 11:14:59 +0100

    feat: P4ADEV-1377 initialize structure (#1)
    * P4ADEV-1377 create structure
    
    * P4ADEV-1377 add dockerfile, application.yml, devops pipeline
    
    * P4ADEV-1377 add helm ignore and SwaggerConfig
    
    * P4ADEV-1377 create structure
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 add openapi example for pipeline error
    
    * P4ADEV-1377 add openapi example for pipeline error
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 change pipeline
    
    * P4ADEV-1377 add lockfile
    
    * P4ADEV-1377 edit helm files and delete devops codereview
    
    * P4ADEV-1377 edit codereview.yml
    
    * P4ADEV-1377 remove PayhubPdndApplicationTests
    
    * P4ADEV-1377 edit security-scan.yml
    
    * P4ADEV-1377 fix sonar error src/test not found
    
    * P4ADEV-1377 update trivy action version
    
    * P4ADEV-1377 edit security-scan.yml

---
+++ src/main/java/it/gov/pagopa/payhub/pdnd/PayhubPdndApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.payhub.pdnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhubPdndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayhubPdndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gov/pagopa/payhub/pdnd/PayhubPdndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.payhub.pdnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayhubPdndApplicationTests {
+
+  @Test
+  public void main() {
+    PayhubPdndApplication.main(new String[] {});
+  }
+}
