ce516d2c0bdb27fa0b9eae67e8834ec6daac2eb8
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2024-11-15 09:52:50 +0100

    add slack web hook

--- .github/terraform/99_locals.tf
+++ .github/terraform/99_locals.tf
@@ -18,7 +18,8 @@ locals {
   env_variables = {}
 
   repo_secrets = var.env_short == "p" ? {
-    SONAR_TOKEN = data.azurerm_key_vault_secret.sonar_token[0].value
+    SONAR_TOKEN       = data.azurerm_key_vault_secret.sonar_token[0].value
+    SLACK_WEBHOOK_URL = data.azurerm_key_vault_secret.slack_webhook[0].value
   } : {}
 
   repo_env = var.env_short == "p" ? {
