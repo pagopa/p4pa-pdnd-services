b3dd11601306e8d16b9008730a2629e60a70226d
antoniocalo <antonio.calo@nttdata.com>
2024-11-21 16:11:19 +0100

    P4ADEV-1341 fix issues

--- src/main/java/it/gov/pagopa/payhub/pdnd/service/PdndClient.java
+++ src/main/java/it/gov/pagopa/payhub/pdnd/service/PdndClient.java
@@ -1,7 +1,12 @@
 package it.gov.pagopa.payhub.pdnd.service;
 
+import com.nimbusds.jose.JOSEException;
 import it.gov.pagopa.payhub.pdnd.dto.AccessTokenDTO;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import org.springframework.web.client.HttpClientErrorException;
 
 public interface PdndClient {
-  AccessTokenDTO getAccessToken() throws Exception;
+  AccessTokenDTO getAccessToken() throws HttpClientErrorException, InvalidKeySpecException, NoSuchAlgorithmException, IOException, JOSEException;
 }
