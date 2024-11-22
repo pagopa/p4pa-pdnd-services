3020f1689c1f52502c202ecb5c2fadb32540596b
antoniocalo <antonio.calo@nttdata.com>
2024-11-22 11:25:15 +0100

    P4ADEV-1341 add JWTUtilsTest

---
+++ src/test/java/it/gov/pagopa/payhub/pdnd/utils/JWTUtilsTest.java
@@ -0,0 +1,42 @@
+package it.gov.pagopa.payhub.pdnd.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+class JWTUtilsTest {
+
+  @Test
+  public void givenValidTokenWhenIsJWTExpiredThenTokenNotExpired() {
+    // Given
+    Date futureDate = new Date(System.currentTimeMillis() + 3600 * 1000); // 1 hour from now
+    String token = JWT.create()
+        .withExpiresAt(futureDate)
+        .sign(com.auth0.jwt.algorithms.Algorithm.HMAC256("secret"));
+
+    // Then
+    assertFalse(JWTUtils.isJWTExpired(token));
+  }
+
+  @Test
+  public void givenExpiredTokenWhenIsJWTExpiredThenTokenExpired() {
+    // Given
+    Date pastDate = new Date(System.currentTimeMillis() - 3600 * 1000); // 1 hour ago
+    String token = JWT.create()
+        .withExpiresAt(pastDate)
+        .sign(com.auth0.jwt.algorithms.Algorithm.HMAC256("secret"));
+    // Then
+    assertTrue(JWTUtils.isJWTExpired(token));
+  }
+
+  @Test
+  public void givenInvalidTokenWhenIsJWTExpiredThenException() {
+    // Given
+    String invalidtoken = "INVALIDTOKEN";
+    // Then
+    assertThrows(JWTDecodeException.class, () -> JWTUtils.isJWTExpired(invalidtoken));
+  }
+}
