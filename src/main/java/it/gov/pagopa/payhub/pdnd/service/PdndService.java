717efe0adc581aaaa2489bdf61bcfcf5c71f661e
antoniocalo <antonio.calo@nttdata.com>
2024-11-22 11:13:56 +0100

    P4ADEV-1341 introduced cache token

--- src/main/java/it/gov/pagopa/payhub/pdnd/service/PdndService.java
+++ src/main/java/it/gov/pagopa/payhub/pdnd/service/PdndService.java
@@ -4,16 +4,21 @@ import com.nimbusds.jose.JOSEException;
 import it.gov.pagopa.payhub.pdnd.connector.pdnd.client.PdndClientImpl;
 import it.gov.pagopa.payhub.pdnd.connector.pdnd.service.PdndClientAssertionBuilderService;
 import it.gov.pagopa.payhub.pdnd.model.PdndGenericConfig;
+import it.gov.pagopa.payhub.pdnd.utils.JWTUtils;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class PdndService {
 
   private final PdndClientImpl pdndClientImpl;
   private final PdndClientAssertionBuilderService pdndClientAssertionBuilderService;
+  private final ConcurrentHashMap<PdndGenericConfig, String> jwtCache = new ConcurrentHashMap<>();
 
   public PdndService(PdndClientImpl pdndClientImpl,
       PdndClientAssertionBuilderService pdndClientAssertionBuilderService) {
@@ -21,9 +26,20 @@ public class PdndService {
     this.pdndClientAssertionBuilderService = pdndClientAssertionBuilderService;
   }
 
-  public String generateToken(PdndGenericConfig pdndGenericConfig)
-      throws InvalidKeySpecException, NoSuchAlgorithmException, IOException, JOSEException {
-    String clientAssertion = pdndClientAssertionBuilderService.buildPdndClientAssertion(pdndGenericConfig);
-    return pdndClientImpl.getAccessToken(pdndGenericConfig.getClientId(), clientAssertion).getAccessToken();
+  public String generateToken(PdndGenericConfig pdndGenericConfig) {
+    return jwtCache.compute(pdndGenericConfig, (key, existingJwt) -> {
+      log.debug("Check cache for token exists and not expired");
+      if(existingJwt == null || JWTUtils.isJWTExpired(existingJwt)) {
+        try {
+          log.debug("Token not present or expired, generate new one");
+          String clientAssertion = pdndClientAssertionBuilderService.buildPdndClientAssertion(key);
+          return pdndClientImpl.getAccessToken(key.getClientId(), clientAssertion).getAccessToken();
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException | IOException | JOSEException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      log.debug("Token is present in cache");
+      return existingJwt;
+    });
   }
 }
