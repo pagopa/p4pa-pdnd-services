126b044280da469cf81251a0b02e4b6621bf9479
antoniocalo <antonio.calo@nttdata.com>
2024-11-25 17:55:43 +0100

    P4ADEV-1341 edit timeout restTemplate

--- src/main/java/it/gov/pagopa/payhub/pdnd/config/RestTemplateConfig.java
+++ src/main/java/it/gov/pagopa/payhub/pdnd/config/RestTemplateConfig.java
@@ -2,20 +2,21 @@ package it.gov.pagopa.payhub.pdnd.config;
 
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Slf4j
 public class RestTemplateConfig {
   @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder builder) {
-      log.debug("settings RestTemplate timeout to 120 sec");
-      return builder
-          .setConnectTimeout(Duration.ofMillis(120000))
-          .setReadTimeout(Duration.ofMillis(120000))
-          .build();
+  public RestTemplate restTemplate() {
+    log.debug("settings RestTemplate timeout to 120 sec");
+    SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    factory.setConnectTimeout(Duration.ofMillis(120000));
+    factory.setReadTimeout(Duration.ofMillis(120000));
+
+    return new RestTemplate(factory);
   }
 }
