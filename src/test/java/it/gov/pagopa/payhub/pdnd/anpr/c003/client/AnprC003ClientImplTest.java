4ea564fa5ca171696d067d4bf9bb3a80b1f15827
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2024-12-06 16:07:04 +0100

    P4ADEV-1429 added junit test

--- src/test/java/it/gov/pagopa/payhub/pdnd/anpr/c003/client/AnprC003ClientImplTest.java
+++ src/test/java/it/gov/pagopa/payhub/pdnd/anpr/c003/client/AnprC003ClientImplTest.java
@@ -1,8 +1,7 @@
-package it.gov.pagopa.payhub.pdnd.anpr.c030.client;
+package it.gov.pagopa.payhub.pdnd.anpr.c003.client;
 
-import it.gov.pagopa.payhub.anpr.C030.model.generated.RichiestaE002;
-import it.gov.pagopa.payhub.anpr.C030.model.generated.RispostaE002OK;
-import it.gov.pagopa.payhub.pdnd.anpr.c030.service.AnprC030ServiceConfig;
+import it.gov.pagopa.payhub.anpr.C003.model.generated.*;
+import it.gov.pagopa.payhub.pdnd.anpr.c003.service.AnprC003ServiceConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -13,12 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-class AnprC030ClientImplTest {
+class AnprC003ClientImplTest {
 
     @Mock
     private RestTemplate restTemplate;
@@ -27,9 +28,9 @@ class AnprC030ClientImplTest {
     private RestTemplateBuilder restTemplateBuilder;
 
     @Mock
-    private AnprC030ServiceConfig anprC030ServiceConfig;
+    private AnprC003ServiceConfig anprC003ServiceConfig;
 
-    private AnprC030ClientImpl anprC030Client;
+    private AnprC003ClientImpl anprC003Client;
 
     @BeforeEach
     void setUp() {
@@ -37,23 +38,41 @@ class AnprC030ClientImplTest {
 
         when(restTemplateBuilder.build()).thenReturn(restTemplate);
 
-        anprC030Client = new AnprC030ClientImpl(restTemplateBuilder, anprC030ServiceConfig);
-        ReflectionTestUtils.setField(anprC030Client, "anprBasePath", "http://localhost:8080");
+        anprC003Client = new AnprC003ClientImpl(restTemplateBuilder, anprC003ServiceConfig);
+        ReflectionTestUtils.setField(anprC003Client, "anprBasePath", "http://localhost:8080");
     }
 
     @Test
-    void testGetIdAnprFromFc() {
+    void getUserData() {
         RichiestaE002 request = RichiestaE002.builder()
                 .idOperazioneClient("13f32508-7bcb-38d0-8510-d68bf240aa59-1733496758205")
                 .build();
 
+        TipoInfoSoggettoEnte subTypeInfo = TipoInfoSoggettoEnte.builder()
+                .id("nome")
+                .chiave("Jed")
+                .valore(TipoInfoValore.S)
+                .valoreTesto("Nome del soggetto")
+                .valoreData("2024-11-02")
+                .dettaglio("")
+                .build();
+
+        TipoDatiSoggettiEnte subDataTypes = TipoDatiSoggettiEnte.builder()
+                .infoSoggettoEnte(List.of(subTypeInfo))
+                .build();
+
+        TipoListaSoggetti subTypeList = TipoListaSoggetti.builder()
+                .datiSoggetto(List.of(subDataTypes))
+                .build();
+
         RispostaE002OK mockResponse = RispostaE002OK.builder()
                 .idOperazioneANPR("ANPR-12345")
+                .listaSoggetti(subTypeList)
                 .build();
 
         ResponseEntity<RispostaE002OK> responseEntity = ResponseEntity.ok(mockResponse);
 
-        when(anprC030ServiceConfig.getUrl()).thenReturn("/test");
+        when(anprC003ServiceConfig.getUrl()).thenReturn("/test");
         when(restTemplate.exchange(
                 eq("http://localhost:8080/test"),
                 eq(HttpMethod.POST),
@@ -61,9 +80,10 @@ class AnprC030ClientImplTest {
                 eq(RispostaE002OK.class)
         )).thenReturn(responseEntity);
 
-        RispostaE002OK result = anprC030Client.getIdAnprFromFc(request);
+        RispostaE002OK result = anprC003Client.getUserData(request);
 
         assertEquals("ANPR-12345", result.getIdOperazioneANPR());
+        assertEquals(1, result.getListaSoggetti().getDatiSoggetto().size());
         verify(restTemplate, times(1)).exchange(
                 eq("http://localhost:8080/test"),
                 eq(HttpMethod.POST),
