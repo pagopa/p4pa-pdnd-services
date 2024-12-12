7ac72d701f063a73fafee7f21457f4ed6b942245
antoniocalo <antonio.calo@nttdata.com>
2024-12-12 09:30:47 +0100

    fix anpr baseurl

--- src/main/java/it/gov/pagopa/payhub/pdnd/anpr/service/PdndServiceImpl.java
+++ src/main/java/it/gov/pagopa/payhub/pdnd/anpr/service/PdndServiceImpl.java
@@ -5,9 +5,11 @@ import it.gov.pagopa.payhub.model.generated.Citizen;
 import it.gov.pagopa.payhub.pdnd.anpr.c003.service.AnprC003Service;
 import it.gov.pagopa.payhub.pdnd.anpr.c030.service.AnprC030Service;
 import it.gov.pagopa.payhub.pdnd.anpr.mapper.AnprResponseMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class PdndServiceImpl implements PdndService {
 
     private final AnprC003Service anprC003Service;
@@ -20,15 +22,16 @@ public class PdndServiceImpl implements PdndService {
 
     @Override
     public Citizen getCitizenData(String fiscalCode) {
+        log.info("retrieve idAnpr");
         RispostaE002OK anprC030Response = anprC030Service.getIdAnprFromFc(fiscalCode);
         String idAnpr = anprC030Response.getListaSoggetti()
                 .getDatiSoggetto()
                 .getFirst()
                 .getIdentificativi()
                 .getIdANPR();
-
+        log.info("idAnpr for fiscalCode {} is {}", fiscalCode, idAnpr);
         it.gov.pagopa.payhub.anpr.C003.model.generated.RispostaE002OK anprC003Response = anprC003Service.getUserData(idAnpr, fiscalCode);
-
+        log.info("userData {}", anprC003Response);
         return AnprResponseMapper.mapToAnprResponse(anprC003Response);
     }
 }
