2cfa2513fa87377adba5ef2fde51da81360baad4
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-24 15:37:37 +0100

    fix: P4ADEV-2136 fix CVE (#24)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.1"
+	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 	jacoco
 	id("org.sonarqube") version "6.0.1.5171"
@@ -28,14 +28,14 @@ repositories {
 	mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.7.0"
+val springDocOpenApiVersion = "2.8.5"
 val openApiToolsVersion = "0.2.6"
-val javaJwtVersion = "4.4.0"
+val javaJwtVersion = "4.5.0"
 val jwksRsaVersion = "0.22.1"
-val wiremockVersion = "3.10.0"
+val wiremockVersion = "3.12.0"
 val wiremockSpringBootVersion = "2.1.3"
-val micrometerVersion = "1.4.1"
-val bouncycastleVersion = "1.79"
+val micrometerVersion = "1.4.3"
+val bouncycastleVersion = "1.80"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
@@ -48,17 +48,13 @@ dependencies {
 	implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
 	implementation("io.micrometer:micrometer-registry-prometheus")
-
-	// validation token jwt
 	implementation("com.auth0:java-jwt:$javaJwtVersion")
 	implementation("com.auth0:jwks-rsa:$jwksRsaVersion")
+	implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testAnnotationProcessor("org.projectlombok:lombok")
-
-	//security
-	implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
 	//	Testing
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
