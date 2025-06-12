16364d5fd8eb0e446838c8c8909b1bb44a60cf30
antonio.torre <antonio.torre@pagopa.it>
2025-06-12 11:24:35 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.5"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
     id("org.sonarqube") version "6.1.0.5360"
@@ -29,16 +29,16 @@ repositories {
     mavenCentral()
 }
 
-val springDocOpenApiVersion = "2.8.6"
+val springDocOpenApiVersion = "2.8.9"
 val openApiToolsVersion = "0.2.6"
 val javaJwtVersion = "4.5.0"
 val jwksRsaVersion = "0.22.1"
 val wiremockVersion = "3.13.0"
 val wiremockSpringBootVersion = "3.10.0"
-val micrometerVersion = "1.4.6"
-val bouncycastleVersion = "1.80"
+val micrometerVersion = "1.5.1"
+val bouncycastleVersion = "1.81"
 val caffeineVersion = "3.2.0"
-val httpClientVersion = "5.4.4"
+val httpClientVersion = "5.5"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
