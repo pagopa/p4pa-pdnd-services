f8956e02aac2bce60d4ab8a2a019dd9ac1768eba
antonio.torre <antonio.torre@pagopa.it>
2025-12-23 19:16:55 +0100

    chore: Logging failure when testing using Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,6 +107,8 @@ dependencies {
 tasks {
     test {
         jvmArgs("-javaagent:${mockitoAgent.asPath}")
+    testLogging.events = setOf(TestLogEvent.FAILED)
+    testLogging.exceptionFormat = TestExceptionFormat.FULL
     }
 }
 
