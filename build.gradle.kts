9def090eaff48fc0f0284e01ac7b847e30a14740
antonio.torre <antonio.torre@pagopa.it>
2026-09-03 10:41:47 +0200

    fix: Fix tomcat CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,9 @@ val lz4JavaVersion = "1.11.2"
 val commonsLang3Version = "3.20.0"
 val podamVersion = "8.0.2.RELEASE"
 
+// CVE Security dependencies
+val tomcatEmbedCoreVersion = "11.0.25"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
@@ -92,6 +95,9 @@ dependencies {
         exclude(group = "org.lz4", module = "lz4-java")
     }
     implementation("at.yawk.lz4:lz4-java:$lz4JavaVersion")
+
+    // CVE Security dependencies
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:$tomcatEmbedCoreVersion")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
